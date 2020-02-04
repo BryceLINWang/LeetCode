b2fbc420b4c4d521982d0efa754fc962e069a6f2
王博林 <Brycewlinw@163.com>
2020-02-04 22:54:55 +0800

    2.4

--- src/Leetcode/环形链表2.java
+++ src/Leetcode/环形链表2.java
@@ -1,5 +1,8 @@
 package Leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 环形链表2 {
     public static class ListNode {
         int val;
@@ -28,8 +31,28 @@ public class 环形链表2 {
 
         ListNode(int x) { val = x; }
     }
-
+    public static ListNode detectCycle(ListNode head) {
+        Set<ListNode> set=new HashSet<ListNode>();
+        ListNode cur=head;
+        while(cur!=null){
+            if (set.contains(cur)){
+                return cur;
+            }
+            set.add(cur);
+            cur=cur.next;
+        }
+        return null;
+    }
     public static void main(String[] args) {
-
+        ListNode l1=new ListNode(3);
+        ListNode l2=new ListNode(2);
+        ListNode l3=new ListNode(0);
+        ListNode l4=new ListNode(-4);
+        l1.setNext(l2);
+        l2.setNext(l3);
+        l3.setNext(l4);
+        l4.setNext(l2);
+        ListNode res=detectCycle(l1);
+        System.out.println(res.val);
     }
 }
