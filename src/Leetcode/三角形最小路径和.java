64dc1e69deaf0de274b1e16b7095641d3dbcfa3a
王博林 <Brycewlinw@163.com>
2020-02-10 23:08:35 +0800

    1111

---
+++ src/Leetcode/三角形最小路径和.java
@@ -0,0 +1,4 @@
+package Leetcode;
+
+public class 三角形最小路径和 {
+}
--- src/Leetcode/最大正方形.java
+++ src/Leetcode/最大正方形.java
@@ -1,7 +1,7 @@
 package Leetcode;
 
 public class 最大正方形 {
-    public int maximalSquare(char[][] matrix) {
+    public static int maximalSquare(char[][] matrix) {
      if(matrix==null||matrix.length==0||matrix[0].length==0){
         return 0;
      }
@@ -12,7 +12,8 @@ public class 最大正方形 {
      for(int row=0;row<height;row++){
          for(int col=0;col<width;col++){
              if(matrix[row][col]=='1'){
-
+              dp[row+1][col+1]=Math.min(Math.min(dp[row][col+1],dp[row+1][col]),dp[row][col])+1;
+              maxvalue=Math.max(maxvalue,dp[row+1][col+1]);
              }
          }
      }
@@ -20,6 +21,7 @@ public class 最大正方形 {
     }
 
     public static void main(String[] args) {
-
+      char [][]aa={{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        System.out.println(maximalSquare(aa));
     }
 }
