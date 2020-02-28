62e2b7e1e5e9c11d0045eb81b556151323a8ed7d
王博林 <Brycewlinw@163.com>
2020-02-28 23:48:59 +0800

    2.28

--- src/Leetcode/盛最多水的容器.java
+++ src/Leetcode/盛最多水的容器.java
@@ -2,10 +2,21 @@ package Leetcode;
 
 public class 盛最多水的容器 {
     public static int maxArea(int[] height) {
-         return 0;
+        int res=0,left=0,right=height.length-1;
+        while(left<right){
+            res=Math.max(res,Math.min(height[left],height[right])*(right-1));
+            if(height[left]<height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return res;
+
     }
 
     public static void main(String[] args) {
         int nums[]={1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(nums));
     }
 }
