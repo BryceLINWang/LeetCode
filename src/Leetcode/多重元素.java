426fdb4c55b1b3d33dafcf68be0175556ac5e76a
王博林 <Brycewlinw@163.com>
2020-03-23 00:01:20 +0800

    re

--- src/Leetcode/多重元素.java
+++ src/Leetcode/多重元素.java
@@ -1,6 +1,22 @@
 package Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class 多重元素 {
+    public static int majorityElement1(int[] nums) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        // 遍历每个数统计次数
+        for (int num: nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+            // 如果某个数次数超过了n/2就返回
+            if (counter.get(num) > nums.length / 2) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
     public int majorityElement(int[] nums) {
         return getMajority(nums, 0, nums.length - 1);
     }
@@ -30,6 +46,7 @@ public class 多重元素 {
 
    
     public static void main(String[] args) {
-
+        int []aa=new int[]{2,2,1,1,1,2,2};
+        System.out.println(majorityElement1(aa));
     }
 }
