305960b72aaba441d52a1b9062aa0383d5ec5582
王博林 <Brycewlinw@163.com>
2020-08-14 22:59:34 +0800

    8.13

--- src/排序/选择.java
+++ src/排序/选择.java
@@ -13,11 +13,15 @@ public class 选择 {
     }
     public static void main(String[] args) {
     int[] arr={5,3,6,8,1,7,9,4,2};
-    int minPos=0;
-    for(int j=1;j<arr.length;j++){
-        if(arr[j]<arr[minPos]){
 
+    for (int i=0;i<arr.length-1;i++) {
+      int  minPos=i;
+        for (int j = i+1; j < arr.length; j++) {
+           minPos=arr[j]<arr[minPos]?j:minPos;
         }
+        int tmp=arr[i];
+        arr[i]=arr[minPos];
+        arr[minPos]=tmp;
     }
     for(int i=0;i<arr.length;i++){
         System.out.println(arr[i]+" ");
