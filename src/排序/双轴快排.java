6ac7ed31cb499b66a0d72f982a4b1c2ae7928c2e
王博林 <Brycewlinw@163.com>
2020-08-17 21:34:31 +0800

    8.17

---
+++ src/排序/双轴快排.java
@@ -0,0 +1,4 @@
+package 排序;
+
+public class 双轴快排 {
+}
--- src/排序/快速.java
+++ src/排序/快速.java
@@ -3,11 +3,29 @@ package 排序;
 public class 快速 {
     public static void main(String[] args) {
         int[] arr={9,6,11,3,5,12,8,7,10,15,14,4,1,13,2};
-        sort(arr);
+        sort(arr,0,arr.length-1);
         print(arr);
     }
-    static void sort(int[] a){
+    static void sort(int[] a,int leftBound,int rightBound){
+     if (leftBound>=rightBound)return;
+     int mid=partition(a,leftBound,rightBound);
+     sort(a,leftBound,mid-1);
+     sort(a,mid+1,rightBound);
+    }
+    static int partition(int[] a,int leftBound,int rightBound){
+      int pivot=a[rightBound];
+      int left=leftBound;
+      int right=rightBound-1;
 
+      while(left<=right){
+          while (left<=right&&a[left]<=pivot)left++;
+          while (left<=right&&a[right]>pivot)right--;
+          //System.out.println("before swap:"+left+" "+right);
+          if (left<right)swap(a,left,right);
+          //print(a);
+      }
+      swap(a,left,rightBound);
+      return left;
     }
     static void swap(int[] a,int i,int j){
         int tmp=a[i];
